5c33514474507c6189555f6eff23394f84906c97
Laura Carolina Zamora <laura.zamora-g@uniminuto.edu.co>
2021-05-16 14:22:58 -0500

    Final Componentes compartido Tab2

---
+++ src/app/pages/people/people.page.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import {SwapiService} from '../../services/swapi.service';
+
+@Component({
+  selector: 'app-people',
+  templateUrl: './people.page.html',
+  styleUrls: ['./people.page.scss'],
+})
+export class PeoplePage implements OnInit {
+
+  people:any;
+  iconname='people';
+
+  constructor(private obtservswapi:SwapiService) { }
+
+  ngOnInit() {
+    this.people=this.obtservswapi.getswapi('people')
+  }
+
+}
---
+++ src/app/pages/planets/planets.page.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import {SwapiService} from '../../services/swapi.service';
+
+@Component({
+  selector: 'app-planets',
+  templateUrl: './planets.page.html',
+  styleUrls: ['./planets.page.scss'],
+})
+export class PlanetsPage implements OnInit {
+  planets:any;
+  iconname = 'planet';
+
+  constructor(private ObtApiService:SwapiService) { }
+
+  ngOnInit() {
+    this.planets=this.ObtApiService.getswapi('planets')
+   /* this.ObtApiService.getswapi('planets').subscribe(data =>{
+      this.planets=data;
+      console.log(data);
+    })
+    */
+  }
+
+}
